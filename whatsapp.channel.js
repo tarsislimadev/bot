64a21608da8b9bc4b54efefab14df0eb61301fdd
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2026-04-21 20:44:46 +0000

    feat: implement WhatsApp client event handling

--- whatsapp.channel.js
+++ whatsapp.channel.js
@@ -1,3 +1,15 @@
 const ee = new EventTarget()
 
+const { Client } = require('whatsapp-web.js')
+
+const client = new Client()
+
+client.on('qr', (qr) => ee.dispatchEvent(new CustomEvent('whatsapp.qr', { detail: qr })))
+
+client.on('ready', (ready) => ee.dispatchEvent(new CustomEvent('whatsapp.ready', { detail: ready })))
+
+client.on('message', (msg) => ee.dispatchEvent(new CustomEvent('whatsapp.message', { detail: msg })))
+
+ee.addEventListener('whatsapp.ee.initialize', (event) => client.initialize())
+
 module.exports = ee
