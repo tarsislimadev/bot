a0b3300d7ac0baed449fdd1dccb504f84afebb0e
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2026-04-21 20:47:18 +0000

    refactor: simplify API key usage in news API integration

--- news.api.js
+++ news.api.js
@@ -2,12 +2,14 @@ const { request } = require('./utils/http.util.js')
 
 const { get_today } = require('./utils/date.util.js')
 
+const apiKey = process.env.NEWS_API_KEY
+
 const news_api = {
   v2: {
-    everything: ({ q, from = get_today(), sortBy = 'popularity' } = {}) =>
-      request({ url: `https://newsapi.org/v2/everything?q=${q}&from=${from}&sortBy=${sortBy}&apiKey=${process.env.NEWS_API_KEY}` }),
-    everything_string: async ({ q, from = get_today(), sortBy = 'popularity' } = {}) => await news_api.v2.everything({ q, from, sortBy }).then(({ articles }) => articles?.map(article => `${article.title} - ${article.url}`).join('\n'))
+    everything: ({ q, from = get_today() } = {}) => request({ url: `https://newsapi.org/v2/everything?q=${q}&from=${from}&sortBy=popularity&apiKey=${apiKey}` }),
   }
 }
 
-module.exports = news_api
+module.exports = {
+  everything_string: async ({ q, from = get_today() } = {}) => await news_api.v2.everything({ q, from }).then(({ articles }) => articles?.map(article => `${article.title} - ${article.url}`).join('\n'))
+}
