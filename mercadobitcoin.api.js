7391b13350303ce82cedc4b8525b54bce2e6c9e2
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2026-04-21 20:48:13 +0000

    refactor: move tickers_string function to module exports for better structure

--- mercadobitcoin.api.js
+++ mercadobitcoin.api.js
@@ -3,8 +3,9 @@ const { request } = require('./utils/http.util.js')
 const mercadobitcoin = {
   v4: {
     tickers: ({ symbols = [] } = {}) => request({ url: `https://api.mercadobitcoin.net/api/v4/tickers?symbols=${symbols.join(',')}` }),
-    tickers_string: ({ symbols = [] } = { symbols }) => mercadobitcoin.v4.tickers({ symbols }).then((items) => ['Buy:', ...items.map(({ pair, buy }) => `${pair} at ${+buy}`)].join('\n'))
   }
 }
 
-module.exports = mercadobitcoin
+module.exports = {
+  tickers_string: ({ symbols = [] } = { symbols }) => mercadobitcoin.v4.tickers({ symbols }).then((items) => ['Buy:', ...items.map(({ pair, buy }) => `${pair} at ${+buy}`)].join('\n'))
+}
